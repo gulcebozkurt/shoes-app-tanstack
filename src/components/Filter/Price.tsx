378b27e51994f4a3ea6f2123d758a1f7b16851e4
gulcebozkurtt <gulceb.design@gmail.com>
2024-08-25 15:21:21 +0300

    first commit

---
+++ src/components/Filter/Price.tsx
@@ -0,0 +1,47 @@
+import { useDebounce } from "@uidotdev/usehooks";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+type Props = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Price = ({ value, setValue }: Props) => {
+  const [params, setParams] = useSearchParams();
+
+  const debouncedValue = useDebounce(value, 300);
+
+  useEffect(() => {
+    if (+debouncedValue > 0) {
+      // seçili cinsiyet varsa aralarına virgül koyup url'e ekle
+      params.set("price", debouncedValue);
+      setParams(params);
+    } else {
+      // seçili cinsiyet yoksa url'de parametreyi kaldır
+      params.delete("price");
+      setParams(params);
+    }
+  }, [debouncedValue]);
+
+  return (
+    <div>
+      <h2 className="font-semibold mb-[16px]">Fiyat</h2>
+
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full"
+        type="range"
+        min={0}
+        max={1000}
+      />
+      <div className="text-xs flex justify-between">
+        <span>${value}</span>
+        <span>$1000</span>
+      </div>
+    </div>
+  );
+};
+
+export default Price;
---
+++ src/pages/Detail/Color.tsx
@@ -0,0 +1,27 @@
+import { colors } from "../../utils/constants";
+
+const Color = ({ data }: { data: string }) => {
+  const arr = data.split(",");
+
+  return (
+    <div>
+      <h2 className="mb-3 font-semibold">Renkler</h2>
+
+      <div className="flex gap-4">
+        {arr.map((item) => {
+          //is'si bilinen elemanın renk koduna erişmek için colors dizisinde elemanı arama
+          const color = colors.find((i) => i.id === item);
+
+          return (
+            <div
+              style={{ background: color?.code || "gray" }}
+              className="h-8 w-8 rounded-full cursor-pointer"
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Color;
